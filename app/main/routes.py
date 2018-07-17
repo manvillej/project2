903282094486fc1fdd2684b610f7a20af0c4b5a2
manvillej <jeffmanvillejr@gmail.com>
2018-07-16 23:57:56 -0400

    added the ajax to be used for channel creation

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 from . import main
 
 @main.route("/")
 def index():
     template='index.html'
     return render_template(template)
+
+@main.route("/api", methods=["GET", "POST"])
+def api():
+	channel = request.form.get("channel")
+	return jsonify({"success": True,"channel":channel})
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,10 +1,37 @@
 document.addEventListener(
-	'DOMContentLoaded', function() {
-   		document.querySelector("#NewUser").onsubmit = () => {
-   			const username = document.querySelector("#username").value
-   			localStorage.setItem('username', username)
+    'DOMContentLoaded', function() {
+        document.querySelector("#NewUser").onsubmit = () => {
+            const username = document.querySelector("#username").value
+            localStorage.setItem('username', username)
 
-   			alert(localStorage.getItem('username'))
-   			return false
-   		}
-	});
+            alert(localStorage.getItem('username'))
+
+            return false
+        }
+
+        document.querySelector("#NewChannel").onsubmit = () => {
+
+            // Initialize new request
+            const request = new XMLHttpRequest();
+            const channel = document.querySelector("#channel").value
+
+            request.open('POST', '/api');
+            
+            // Callback function for when request completes
+            request.onload = () => {
+                const data = JSON.parse(request.responseText);
+
+                if(data.success) {
+                    alert(data.channel);
+                } else {
+                    alert("Fail");
+                }
+            }
+
+            const data = new FormData();
+            data.append('channel', channel);
+
+            request.send(data);
+            return false
+        }
+    });
